96e441a06f0cb875e27402cbe2cd9170bb73fce1
DorotaStaszczuk <dorota.staszczuk@gmail.com>
2022-03-27 14:39:50 +0200

    recipe-form component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeTileComponent } from './recipe-tile/recipe-tile.component';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
+import { RecipeFormComponent } from './recipe-form/recipe-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipeListComponent,
     RecipeTileComponent,
-    RecipeDetailsComponent
+    RecipeDetailsComponent,
+    RecipeFormComponent
   ],
   imports: [
     BrowserModule
