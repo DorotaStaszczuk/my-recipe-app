714171be03fe0dc3d6919733af3941b04537e934
DorotaStaszczuk <dorota.staszczuk@gmail.com>
2022-03-27 14:38:53 +0200

    recipe-details component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeTileComponent } from './recipe-tile/recipe-tile.component';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipeListComponent,
-    RecipeTileComponent
+    RecipeTileComponent,
+    RecipeDetailsComponent
   ],
   imports: [
     BrowserModule
