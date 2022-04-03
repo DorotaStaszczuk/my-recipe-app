3c332c045d7c6945d3b17758d7828a0085f6745f
DorotaStaszczuk <dorota.staszczuk@gmail.com>
2022-04-03 14:13:39 +0200

    rating pipe working

--- src/app/rating.pipe.ts
+++ src/app/rating.pipe.ts
@@ -1,12 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'rating'
+  name: 'rating',
 })
 export class RatingPipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(rating: number) {
+    return '⭐'.repeat(rating);
   }
-
 }
