90ef2fe2e790567c20ec3767c43e386296e09272
dorota.staszczuk <dorota.staszczuk@ppuc.pl>
2022-04-09 19:39:42 +0200

    createForm method added with form structure

--- src/app/recipe-form.service.ts
+++ src/app/recipe-form.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { FormArray, FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeFormService {
+  constructor(private formBuilder: FormBuilder) {}
 
-  constructor() { }
+  public createForm() {
+    const recipeForm = this.formBuilder.group({
+      recipeName: ['', [Validators.required, Validators.minLength(3)]],
+      recipeDescription: ['', [Validators.required, Validators.minLength(10)]],
+      ingredients: this.formBuilder.array([
+        this.formBuilder.group({
+          ingredientName: ['', Validators.required],
+          ingredientAmount: ['', Validators.required],
+        }),
+      ]),
+    });
+
+    return recipeForm;
+  }
 }
