f7d0ba626a39eea0f7f5bb9ec7a51f2434344a07
DorotaStaszczuk <dorota.staszczuk@gmail.com>
2022-04-10 11:57:48 +0200

    recipe-list working
    Recipe and Ingredient interface added, sort options, sort and filter controls added, subscribing to all recipes, sorted and filtered recipes

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,15 +1,87 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs';
+import { RecipeApiService } from './recipe-api.service';
+export interface Recipe {
+  name: string;
+  id: number;
+  description: string[];
+  rating: number;
+  ingredients: Ingredient[];
+}
+export interface Ingredient {
+  name: string;
+  value: string;
+}
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
+  public recipes: Recipe[] = [];
+  constructor(private recipeApiService: RecipeApiService) {}
 
-  constructor() { }
+  sortOptions = [
+    {
+      value: 'name,asc',
+      label: 'po nazwie rosnąco',
+    },
+    {
+      value: 'name,desc',
+      label: 'po nazwie malejąco',
+    },
+    {
+      value: 'rating,asc',
+      label: 'po ocenie rosnąco',
+    },
+    {
+      value: 'rating,desc',
+      label: 'po ocenie malejąco',
+    },
+  ];
 
-  ngOnInit(): void {
+  sortControl = new FormControl('');
+  filterControl = new FormControl('');
+
+  ngOnInit() {
+    this.recipeApiService.getRecipes().subscribe((result) => {
+      this.recipes = [...result];
+    });
+
+    this.sortControl.valueChanges
+      .pipe(map((value: string) => value.split(',')))
+      .subscribe(([propName, sortValue]) => {
+        if (propName === 'name' && sortValue === 'desc') {
+          this.recipeApiService.sortRecipesByNameDesc().subscribe((result) => {
+            this.recipes = [...result];
+          });
+        }
+        if (propName === 'name' && sortValue === 'asc') {
+          this.recipeApiService.sortRecipesByNameAsc().subscribe((result) => {
+            this.recipes = [...result];
+          });
+        }
+        if (propName === 'rating' && sortValue === 'desc') {
+          this.recipeApiService
+            .sortRecipesByRatingDesc()
+            .subscribe((result) => {
+              this.recipes = [...result];
+            });
+        }
+        if (propName === 'rating' && sortValue === 'asc') {
+          this.recipeApiService.sortRecipesByRatingAsc().subscribe((result) => {
+            this.recipes = [...result];
+          });
+        }
+      });
+
+    this.filterControl.valueChanges
+      .pipe(debounceTime(400), distinctUntilChanged())
+      .subscribe((value: string) => {
+        this.recipeApiService.filter(value).subscribe((result) => {
+          this.recipes = [...result];
+        });
+      });
   }
-
 }
