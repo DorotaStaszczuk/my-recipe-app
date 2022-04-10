a5f6a35855529bae037ed2aea78bb63d40894a3e
DorotaStaszczuk <dorota.staszczuk@gmail.com>
2022-04-10 11:47:35 +0200

    get, sort and filter methods added

--- src/app/recipe-list/recipe-api.service.ts
+++ src/app/recipe-list/recipe-api.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Recipe } from './recipe-list.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeApiService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  public getRecipes() {
+    return this.http.get<Recipe[]>('http://localhost:3000/recipes');
+  }
+
+  public sortRecipesByRatingAsc() {
+    return this.http.get<Recipe[]>(
+      'http://localhost:3000/recipes?_sort=rating&_order=asc'
+    );
+  }
+
+  public sortRecipesByRatingDesc() {
+    return this.http.get<Recipe[]>(
+      'http://localhost:3000/recipes?_sort=rating&_order=desc'
+    );
+  }
+
+  public sortRecipesByNameAsc() {
+    return this.http.get<Recipe[]>(
+      'http://localhost:3000/recipes?_sort=name&_order=asc'
+    );
+  }
+
+  public sortRecipesByNameDesc() {
+    return this.http.get<Recipe[]>(
+      'http://localhost:3000/recipes?_sort=name&_order=desc'
+    );
+  }
+
+  public filter(filterValue: string) {
+    return this.http.get<Recipe[]>(
+      'http://localhost:3000/recipes?name_like=' + filterValue
+    );
+  }
 }
