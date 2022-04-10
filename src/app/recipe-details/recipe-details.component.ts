2539f4e06ef5e2f60e93a2c3e9ff1baec98cb211
DorotaStaszczuk <dorota.staszczuk@gmail.com>
2022-04-10 12:23:18 +0200

    displaying selected recipe details

--- src/app/recipe-details/recipe-details.component.ts
+++ src/app/recipe-details/recipe-details.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe-list/recipe-list.component';
+import { Ingredient } from '../recipe-list/recipe-list.component';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.component.html',
-  styleUrls: ['./recipe-details.component.scss']
+  styleUrls: ['./recipe-details.component.scss'],
 })
 export class RecipeDetailsComponent implements OnInit {
+  public recipe?: Recipe;
+  public ingredients!: Ingredient[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.recipeService.selectedRecipe.subscribe((value) => {
+      this.recipe = value;
+      this.ingredients = value.ingredients;
+    });
   }
-
 }
