4bab4c3985a323404f7f560392095426518ef4f1
DorotaStaszczuk <dorota.staszczuk@gmail.com>
2022-04-17 19:49:22 +0200

    routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
+import { RecipeFormComponent } from './recipe-form/recipe-form.component';
 
-
+const routes: Routes = [
+  {
+    path: 'details',
+    component: RecipeDetailsComponent,
+    canActivate: [],
+  },
+  {
+    path: 'details/:id',
+    component: RecipeDetailsComponent,
+    canActivate: [],
+  },
+  {
+    path: 'form',
+    component: RecipeFormComponent,
+    canActivate: [],
+  },
+  {
+    path: '',
+    redirectTo: 'details',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'details',
+  },
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
